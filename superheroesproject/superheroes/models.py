55d6d53553bc119f4cc50e58db89f25d55d53761
Paul-Longo <imanazarite88@gmail.com>
2021-07-30 11:13:02 -0400

    new Pipfile, pipfile.lock, superheroesproject files, and superhero app files.

---
+++ superheroesproject/superheroes/models.py
@@ -0,0 +1,15 @@
+from typing_extensions import runtime
+from django.db import models
+
+# Create your models here.
+
+
+class Superhero(models.Model):
+    superhero_name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    primary_super_power = models.CharField(max_length=100)
+    secondary_super_power = models.CharField(max_length=100)
+    catchphrase = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.superhero_name
