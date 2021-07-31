2a35d5fab5fb8299b46bb9ceea65d0994f5ae3b0
Paul-Longo <imanazarite88@gmail.com>
2021-07-30 20:14:09 -0400

    added <a>tag to index.html file. added path in superheroes urls.py file. added method to superheroes views.py file.

--- superheroesproject/superheroes/views.py
+++ superheroesproject/superheroes/views.py
@@ -9,3 +9,10 @@ def index(request):
         'all_superheroes': all_superheroes
     }
     return render(request, 'superheroes/index.html', context)
+
+def detail(request, superhero_id):
+    single_superhero = Superhero.objects.get(pk=superhero_id)
+    context = {
+        'single_superhero': single_superhero
+    } 
+    return render(request, 'superheroes/detail.html', context)
