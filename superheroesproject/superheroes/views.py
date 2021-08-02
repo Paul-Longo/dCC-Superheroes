ce20916a6b503257d09f16d228edb980ad8d412d
Paul-Longo <imanazarite88@gmail.com>
2021-08-02 10:03:58 -0400

    views.py file updated. made a update.html file.

--- superheroesproject/superheroes/views.py
+++ superheroesproject/superheroes/views.py
@@ -30,3 +30,24 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def update(request, superhero_id):
+    if request.method == 'POST':
+        updated_hero = Superhero.objects.get(pk=superhero_id)
+        updated_hero.superhero_name = request.Post.get('superhero_name')
+        updated_hero.alter_ego = request.Post.get('alter_ego')
+        updated_hero.primary_super_power = request.Post.get('primary_super_power')
+        updated_hero.secondary_super_power = request.Post.get('secondary_super_power')
+        updated_hero.catchphrase = request.Post.get('catchphrase')
+        updated_hero.save()
+        return detail(request, superhero_id)
+    else:
+        superhero = Superhero.objects.get(pk=superhero_id)
+        context = {
+            'superhero': superhero
+        }
+        return render(request, 'superheroes/update.html', context)
+
+def delete(request, superhero_id):
+    Superhero.objects.filter(pk=superhero_id).delete()
+    return HttpResponseRedirect(reverse('superheroes:index'))
