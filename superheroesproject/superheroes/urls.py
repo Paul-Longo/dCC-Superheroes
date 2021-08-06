3c95c4b4820cfe0251538c9c6c6f4e66c90ba814
Paul-Longo <imanazarite88@gmail.com>
2021-08-05 21:19:18 -0400

    added a path for updat and delete in superheroes urls.py

--- superheroesproject/superheroes/urls.py
+++ superheroesproject/superheroes/urls.py
@@ -6,5 +6,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create_new_superhero')
+    path('new/', views.create, name='create_new_superhero'),
+    path('update/', views.update, name='updated_superhero'),
+    path('delete/', views.delete, name='delete')
 ]
