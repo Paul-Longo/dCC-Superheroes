e65095de418c01eb79ed047cc090ef38e882918f
Paul-Longo <imanazarite88@gmail.com>
2021-09-21 17:22:34 -0400

    added imports and static

--- superheroesproject/superheroes/urls.py
+++ superheroesproject/superheroes/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'superheroes'
@@ -7,6 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.detail, name='detail'),
     path('new/', views.create, name='create_new_superhero'),
-    path('update/<int:superhero_id>/', views.update, name='updated_superhero'),
+    path('update/<int:superhero_id>/', views.update, name='update_superhero'),
     path('delete/<int:superhero_id>/', views.delete, name='delete')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
