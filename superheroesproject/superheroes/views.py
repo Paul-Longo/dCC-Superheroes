9c316316bc9823f5ea46041c3081ad1133ba6a15
Paul-Longo <imanazarite88@gmail.com>
2021-09-21 17:32:58 -0400

    updated code in create and update methods

--- superheroesproject/superheroes/views.py
+++ superheroesproject/superheroes/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from .models import Superhero
 # Create your views here.
 
+
 def index(request):
     all_superheroes = Superhero.objects.all()
     context = {
@@ -11,42 +12,49 @@ def index(request):
     }
     return render(request, 'superheroes/index.html', context)
 
+
 def detail(request, superhero_id):
     single_superhero = Superhero.objects.get(pk=superhero_id)
     context = {
         'single_superhero': single_superhero
-    } 
+    }
     return render(request, 'superheroes/detail.html', context)
+
 
 def create(request):
     if request.method == 'POST':
-        superhero_name = request.Post.get('superhero_name')
-        alter_ego = request.Post.get('alter_ego')
-        primary_super_power = request.Post.get('primary_super_power')
-        secondary_super_power = request.Post.get('secondary_super_power')
-        catchphrase = request.Post.get('catchphrase')
-        new_superhero = Superhero(superhero_name=superhero_name, alter_ego=alter_ego, primary_super_power=primary_super_power, secondary_super_power=secondary_super_power, catchphrase=catchphrase)
+        superhero_name = request.POST.get('superhero_name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_super_power = request.POST.get('primary_super_power')
+        secondary_super_power = request.POST.get('secondary_super_power')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(superhero_name=superhero_name, alter_ego=alter_ego, primary_super_power=primary_super_power,
+                                  secondary_super_power=secondary_super_power, catchphrase=catchphrase)
         new_superhero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
 
+
 def update(request, superhero_id):
     if request.method == 'POST':
-        updated_hero = Superhero.objects.get(pk=superhero_id)
-        updated_hero.superhero_name = request.Post.get('superhero_name')
-        updated_hero.alter_ego = request.Post.get('alter_ego')
-        updated_hero.primary_super_power = request.Post.get('primary_super_power')
-        updated_hero.secondary_super_power = request.Post.get('secondary_super_power')
-        updated_hero.catchphrase = request.Post.get('catchphrase')
-        updated_hero.save()
+        update_hero = Superhero.objects.get(pk=superhero_id)
+        update_hero.superhero_name = request.POST.get('superhero_name')
+        update_hero.alter_ego = request.POST.get('alter_ego')
+        update_hero.primary_super_power = request.POST.get(
+            'primary_super_power')
+        update_hero.secondary_super_power = request.POST.get(
+            'secondary_super_power')
+        update_hero.catchphrase = request.POST.get('catchphrase')
+        update_hero.save()
         return detail(request, superhero_id)
     else:
-        superhero = Superhero.objects.get(pk=superhero_id)
+        update_hero = Superhero.objects.get(pk=superhero_id)
         context = {
-            'superhero': superhero
+            'update_hero': update_hero
         }
         return render(request, 'superheroes/update.html', context)
+
 
 def delete(request, superhero_id):
     Superhero.objects.filter(pk=superhero_id).delete()
